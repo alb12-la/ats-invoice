683dce921701a16d15ec2a6cff828a60b40865ef
alb <alberto@nestegg.rent>
2023-07-10 14:01:01 -0700

    Change date input format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbDatepickerModule, NgbModalModule, NgbModule, NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerModule, NgbModalModule, NgbModule, NgbTimepickerModule, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 import { InvoiceDisplayComponent } from './components/invoice-display/invoice-display.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { customNgbDateFRParserFormatter } from './ngb-date-fr-parser-formatter'
 import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
@@ -28,7 +29,8 @@ import { APP_BASE_HREF } from '@angular/common';
     NgbModalModule,
   ],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/ats-invoice' }
+    { provide: APP_BASE_HREF, useValue: '/ats-invoice' },
+    { provide: NgbDateParserFormatter, useClass: customNgbDateFRParserFormatter }
   ],
   bootstrap: [AppComponent]
 })
